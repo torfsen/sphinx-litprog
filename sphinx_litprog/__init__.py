921553cc12bbc77675f3dcbad3e25b02f33f3b90
Torf <mail@torf.cc>
2019-03-07 21:55:37 +0100

    Add litprog builder

--- sphinx_litprog/__init__.py
+++ sphinx_litprog/__init__.py
@@ -3,15 +3,53 @@ A literate programming extension for Sphinx.
 '''
 
 import os.path
-import os
 
-from docutils import nodes
-from docutils.parsers.rst import Directive, directives
+from docutils.parsers.rst import directives
 
+from sphinx.builders import Builder
 from sphinx.directives.code import CodeBlock
 
 
 __version__ = '0.1.0'
+
+
+def _docnames_in_toc_order(env):
+    '''
+    Yields all docnames in depth-first TOC order.
+    '''
+    includes = env.toctree_includes
+    stack = [env.config.master_doc]
+    while stack:
+        docname = stack.pop()
+        yield docname
+        stack.extend(includes.get(docname, []))
+
+
+class LitProgBuilder(Builder):
+    name = 'litprog'
+
+    def get_outdated_docs(self):
+        return self.env.found_docs
+
+    def get_target_uri(self, *args, **kwargs):
+        return ''
+
+    def prepare_writing(self, *args, **kwargs):
+        return
+
+    def write_doc(self, *args, **kwargs):
+        return
+
+    def finish(self):
+        config = self.app.config
+        env = self.app.env
+        snippets = _get_snippets(env)
+        filename = os.path.join(self.outdir, config.litprog_filename)
+        with open(filename, 'w', encoding='utf-8') as f:
+            for docname in _docnames_in_toc_order(env):
+                doc_snippets = snippets.get(docname, [])
+                if doc_snippets:
+                    f.write('\n'.join(doc_snippets) + '\n')
 
 
 def _get_snippets(env):
@@ -20,7 +58,7 @@ def _get_snippets(env):
     return env.litprog_snippets
 
 
-class LitProg(CodeBlock):
+class LitProgDirective(CodeBlock):
     '''
     Literate programming directive.
     '''
@@ -43,43 +81,22 @@ class LitProg(CodeBlock):
             # Don't produce output in the documentation
             return []
 
-        return super(LitProg, self).run()
+        return super().run()
 
 
 def _purge_doc_snippets(app, env, docname):
     _get_snippets(env).pop(docname, None)
 
 
-def _write_output(app, exception):
-    if exception:
-        return
-    config = app.config
-    env = app.env
-    master_doc = config.master_doc
-    includes = env.toctree_includes
-    snippets = _get_snippets(env)
-
-    filename = os.path.join(app.outdir, config.litprog_filename)
-    print('Writing literate programming code to {}'.format(os.path.relpath(filename)))
-
-    with open(filename, 'w', encoding='utf-8') as f:
-
-        def visit(docname):
-            doc_snippets = snippets.get(docname, [])
-            if doc_snippets:
-                f.write('\n'.join(doc_snippets) + '\n')
-            for include in includes.get(docname, []):
-                visit(include)
-
-        visit(master_doc)
-
-
 def setup(app):
-    app.add_directive('litprog', LitProg)
+    app.add_builder(LitProgBuilder)
+    app.add_directive('litprog', LitProgDirective)
     app.connect('env-purge-doc', _purge_doc_snippets)
-    app.connect('build-finished', _write_output)
     app.add_config_value('litprog_filename', 'litprog.py', '')
     return {
         'version': __version__,
+        'env_version': 1,
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
     }
 
